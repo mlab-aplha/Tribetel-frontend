1be53f2c70562264909034398a841bcf5bf5da3f
nomala03 <mbalimadonsela03@gmail.com>
2025-10-20 02:42:09 +0200

    feat(signin) add bg-image & logo, add gradient overlay on the bg-image, add font family as link on index.html

--- tribetel-frontend/src/components/layout/SignIn/SignInLayout.tsx
+++ tribetel-frontend/src/components/layout/SignIn/SignInLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react"; 
 import "./SignInLayout.css"; 
+import logo from "../../../assets/tribtel-logo.png";
 
 interface SignInLayoutProps {
   children: React.ReactNode;
@@ -10,7 +11,7 @@ const SignInLayout: React.FC<SignInLayoutProps> = ({ children }) => {
     <div className="signin-container">
       {/* Left side - Branding */}
       <div className="signin-left">
-        <img src="" alt="Tribtel-logo" className="signin-logo" />
+        <img src={logo} alt="Tribtel-logo" className="signin-logo" />
         <h1 className="signin-brand">Tribtel</h1>
       </div>
 
