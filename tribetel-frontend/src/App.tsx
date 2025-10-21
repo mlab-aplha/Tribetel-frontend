dbb9f39b2993a98d39977be1ce19fced9ce11b0d
nomala03 <mbalimadonsela03@gmail.com>
2025-10-21 02:20:27 +0200

    feat: debugging navigation

--- tribetel-frontend/src/App.tsx
+++ tribetel-frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SignInPage from "./pages/SignInPage/SignInPage";
 
+
 function App() {
-    return (
-        <div className="App">
-            <SignInPage />
-        </div>
-             
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="*" element={<p>404 Page Not Found</p>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
