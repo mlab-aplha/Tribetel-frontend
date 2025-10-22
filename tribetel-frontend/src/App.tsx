a03ec91bf5bb775d3a5a9f274ef2180e25fba29e
nomala03 <mbalimadonsela03@gmail.com>
2025-10-22 14:44:35 +0200

    fixing layout

--- tribetel-frontend/src/App.tsx
+++ tribetel-frontend/src/App.tsx
@@ -1,16 +1,15 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import AdminSignInForm from "./components/layout/AdminSignIn/AdminSignInForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminSignInForm from "./components/layout/AdminSignIn/AdminSignInForm";
 
 
 function App() {
   return (
-    <h2> Hello </h2>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/admin-signin" element={<AdminSignInForm />} />
-    //     <Route path="*" element={<p>404 Page Not Found</p>} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/admin-signin" element={<AdminSignInForm />} />
+        <Route path="*" element={<p>404 Page Not Found</p>} />
+      </Routes>
+    </Router>
   )  
 }
 
--- tribetel-frontend/src/pages/AdminSignInPage/AdminSignInPage.tsx
+++ tribetel-frontend/src/pages/AdminSignInPage/AdminSignInPage.tsx
@@ -1,12 +1,12 @@
-// import AdminSignInForm from "../../components/layout/AdminSignIn/AdminSignInForm";
+import AdminSignInForm from "../../components/layout/AdminSignIn/AdminSignInForm";
 
 
-// const AdminSignInPage: React.FC = () => {
-//   return (
-//     <div className="admin-signin-page"> 
-//         <AdminSignInForm />
-//     </div>
-//   );
-// }   
+const AdminSignInPage: React.FC = () => {
+  return (
+    <div className="admin-signin-page"> 
+        <AdminSignInForm />
+    </div>
+  );
+}   
 
-// export default AdminSignInPage;
+export default AdminSignInPage;
