887aa6087ab8651e4c4426d025640f751d854197
nomala03 <mbalimadonsela03@gmail.com>
2025-10-22 14:30:59 +0200

    edit:image-bg, margin-top to logo

--- tribetel-frontend/src/pages/AdminSignInPage/AdminSignInPage.tsx
+++ tribetel-frontend/src/pages/AdminSignInPage/AdminSignInPage.tsx
@@ -2,8 +2,7 @@ import AdminLayout from "../../components/layout/AdminSignIn/AdminLayout";
 
 const AdminSignInPage = () => {
   return <AdminLayout>
-    <h2>Admin Sign-In Page</h2>
-    {/* Sign-in form can be added here in the future */}
+    
   </AdminLayout>;
 }   
 export default AdminSignInPage;
