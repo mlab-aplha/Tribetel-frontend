3cb022c6b038db71c9ff3b24ae5f7bd20c66dd51
nomala03 <mbalimadonsela03@gmail.com>
2025-10-23 09:38:04 +0200

    removed imports

--- tribetel-frontend/src/App.tsx
+++ tribetel-frontend/src/App.tsx
@@ -1,22 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import AdminSignInPage from "./pages/AdminSignInPage/AdminSignInPage";
-import SuccessScreen from "./pages/SuccessScreen/SuccessScreen";
-
+import React from 'react';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/signin" replace />} />
-                <Route path="/signin" element={<SignInPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/success" element={<SuccessScreen />} />
-                <Route path="/admin/signin" element={<AdminSignInPage />} />
-            </Routes>
-        </Router>
+       <h2>Hello World</h2>
     
     );
 }
