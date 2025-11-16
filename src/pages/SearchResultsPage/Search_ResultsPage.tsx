685803898063871c71177fa06821dd780d712ebc
mbuso.mulaudzi50@gmail.com <youremail@example.com>
2025-11-16 08:49:32 +0200

    ;;;

--- src/pages/SearchResultsPage/Search_ResultsPage.tsx
+++ src/pages/SearchResultsPage/Search_ResultsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from "./Search_ResultsPage.module.css";
 import type { HotelSummary } from "../../../src/components/types/common";
-import SearchBar from "../../components/common/Searchbar/SearchBar";
+import SearchBar from "@/components/common/Searchbar/SearchBar";
 
 const sampleHotels: HotelSummary[] = [
     {
