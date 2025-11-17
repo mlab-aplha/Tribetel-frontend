772fbd7fd43294e7062c43212ca1af24bc8f1387
Plutoball <118963278+Plutoball@users.noreply.github.com>
2025-11-17 22:21:28 +0200

    Image optimisation

--- src/components/features/hero/HeroSection/HeroSection.tsx
+++ src/components/features/hero/HeroSection/HeroSection.tsx
@@ -4,8 +4,8 @@ import Button from '@components/common/Button/Button';
 import Card from '@components/common/Card/Card';
 import styles from './HeroSection.module.css';
 import heroImage from '@/assets/hero-image.svg';
-import service1 from '@/assets/service1.png';
-import service2 from '@/assets/service2.png';
+import service1 from '@/assets/Hotel_2.svg';
+import service2 from '@/assets/Hotel_3.svg';
 import service3 from '@/assets/service3.png';
 
 const mockUser = {
@@ -550,3 +550,4 @@ const HeroSection: React.FC = () => {
 
 
 export default HeroSection;
+
